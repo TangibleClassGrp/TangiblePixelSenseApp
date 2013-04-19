da1cc09cb6c3db98c1c4ed51797981ecdbab9ef2
CJT <cjtalbot@unknown.github.com>
2013-04-19 14:44:33 -0400

    Redesign work which includes background, clipart drag/drop, and additional image visibility
    Made background static and in the background
    Added function for menu to clear all scatterviewitems
    Added function for menu to change background of main window
    Incorporated all new images and reorganization into project
    Added functionality to drag / drop clipart items - need to check
    boundaries
    Added function to drag clipart back to librarybar, but needs to be
    tested further

--- Application/MyScene/SurfaceWindow1.xaml.cs
+++ Application/MyScene/SurfaceWindow1.xaml.cs
@@ -24,6 +24,10 @@ namespace MyScene
     /// </summary>
     public partial class SurfaceWindow1 : SurfaceWindow
     {
+        int curIndex = 0;
+        List<ScatterViewItem> curClipArt = new List<ScatterViewItem>();
+
+
         /// <summary>
         /// Default constructor.
         /// </summary>
@@ -34,35 +38,53 @@ namespace MyScene
             // Add handlers for window availability events
             AddWindowAvailabilityHandlers();
 
+
+
             // CJT Add all Background Images to the default Background Selection Box
             //var mystring = MyScene.App.Current.Resources.ToString();
-                //ResourceAssembly.Location.ToString();
-            string[] BackgroundFileNames = Directory.GetFiles(Directory.GetCurrentDirectory()+@"\..\..\Resources\Images\Backgrounds\", "*.png");
-            //var mystring = Directory.GetCurrentDirectory();
-            for (int i=0; i < BackgroundFileNames.Length; i++) 
-            {
-               BackgroundList.Items.Add(BackgroundFileNames[i]);
-            }
+            //ResourceAssembly.Location.ToString();
+            /*  string[] BackgroundFileNames = Directory.GetFiles(Directory.GetCurrentDirectory()+@"\..\..\Resources\Images\Backgrounds\", "*.png");
+              //var mystring = Directory.GetCurrentDirectory();
+              for (int i=0; i < BackgroundFileNames.Length; i++) 
+              {
+                 BackgroundList.Items.Add(BackgroundFileNames[i]);
+              }*/
             // CJT should add a LibraryBar.SetIsItemDataEnabled(whichitem, false); to disable what is currently on the main window
 
             // CJT Add all the ClipArt Images to the default ClipArt Selection Box
-            string[] ClipArtFileNames = Directory.GetFiles(Directory.GetCurrentDirectory()+@"\..\..\Resources\Images\ClipArt\", "*.png");
-            for (int j = 0; j < ClipArtFileNames.Length; j++)
+            string[] curDir = Directory.GetDirectories(Directory.GetCurrentDirectory() + @"\..\..\Resources\Images\ClipArt\");
+            string[] ClipArtFileNames = null;
+            if (curDir != null && curDir.Length != 0)
             {
-                ClipArtList.Items.Add(ClipArtFileNames[j]);
-                ClipArtList1.Items.Add(ClipArtFileNames[j]);
-                ClipArtList2.Items.Add(ClipArtFileNames[j]);
-                ClipArtList3.Items.Add(ClipArtFileNames[j]);
-                ClipArtList4.Items.Add(ClipArtFileNames[j]);
-                ClipArtList5.Items.Add(ClipArtFileNames[j]);
-                ClipArtList6.Items.Add(ClipArtFileNames[j]);
-                ClipArtList7.Items.Add(ClipArtFileNames[j]);
-                ClipArtList8.Items.Add(ClipArtFileNames[j]);
-                ClipArtList9.Items.Add(ClipArtFileNames[j]);
-                ClipArtList10.Items.Add(ClipArtFileNames[j]);
-            }
+                for (int i = 0; i < curDir.Length; i++)
+                {
 
-            
+                    ClipArtFileNames = Directory.GetFiles(curDir[i]+@"\", "*.png");
+                    for (int j = 0; j < ClipArtFileNames.Length; j++)
+                    {
+                        ClipArtList.Items.Add(ClipArtFileNames[j]);
+                        /*ClipArtList1.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList2.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList3.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList4.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList5.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList6.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList7.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList8.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList9.Items.Add(ClipArtFileNames[j]);
+                        ClipArtList10.Items.Add(ClipArtFileNames[j]);*/
+                    }
+                }
+            }
+          /*  ClipArtFileNames = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\..\..\Resources\Images\ClipArt\", "*.png");
+            for (int k = 0; k < ClipArtFileNames.Length; k++)
+            {
+                ClipArtList.Items.Add(ClipArtFileNames[k]);
+                
+            }*/
+            // above not needed since adding as resources took care of moving all the files out of the main directory and now makes everything a dupe
+
+
 
         }
 
@@ -76,6 +98,48 @@ namespace MyScene
 
             // Remove handlers for window availability events
             RemoveWindowAvailabilityHandlers();
+        }
+
+        // CJT Added for changing the background image after clicking it on the menu
+        private void changeBackground(object sender, SurfaceDragDropEventArgs e) // check what the params are for a menu click!!
+        {
+            // find the path information for the dragged object
+            FrameworkElement findSource = (e.Cursor.Visual) as FrameworkElement;
+            Image draggedElement = null;
+            while (draggedElement == null && findSource != null)
+            {
+                if ((draggedElement = findSource as Image) == null)
+                {
+                    findSource = VisualTreeHelper.GetChild(findSource, 0) as FrameworkElement;
+                }
+            }
+
+            if (draggedElement == null)
+            {
+                return;
+            }
+
+            string ImagePath = draggedElement.Source.ToString();
+
+            // create new image object for the dragged object
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(ImagePath, UriKind.RelativeOrAbsolute));
+            img.Stretch = System.Windows.Media.Stretch.UniformToFill;
+            // overwrite the Main Window with this new image
+            scatterView.Items.Remove(MyMainWin.Content);
+            MyMainWin.Content = img;
+            // do with LibraryBar.SetIsItemDataEnabled(whichitem, true); -- see http://social.msdn.microsoft.com/Forums/en-US/surfaceappdevelopment/thread/8d341171-8ae9-4ccc-8e5c-84a3aa8a4d29/
+
+        }
+
+        // CJT Added for clearing the screen
+        private void clearScreen(object sender, SurfaceDragDropEventArgs e) // check the params for the menu click
+        {
+            foreach (ScatterViewItem img in curClipArt)
+            {
+                scatterView.Items.Remove(img); // remove from screen
+                curClipArt.Remove(img); // remove from list
+            }
         }
 
         // CJT Added for swapping the background images on the main window
@@ -112,9 +176,42 @@ namespace MyScene
             else
             {
                 // do logic for dropping clipart items!!!
+                Image caimg = new Image();
+                caimg.Source = new BitmapImage(new Uri(ImagePath, UriKind.RelativeOrAbsolute));
+                caimg.Stretch = System.Windows.Media.Stretch.Fill;
+                //caimg.Parent = MyMainWin;
+                //caimg.Tag = "ClipArtDraggedItem";
+
+                ScatterViewItem casvi = new ScatterViewItem();
+                casvi.Background = System.Windows.Media.Brushes.Transparent;
+                casvi.MaxHeight = 1420;
+                casvi.MaxWidth = 580;
+                casvi.Height = caimg.Source.Height / 10;
+                casvi.Width = caimg.Source.Width / 10;
+                casvi.MinHeight = 60;
+                casvi.MinWidth = 60;
+                //casvi.Parent = MyMainWin;
+                casvi.Tag = "ClipArtDraggedItem";
+                casvi.Content = caimg;
+                //casvi.BorderThickness = System.Windows.Thickness(0,0,0,0);
+                casvi.CanMove = true;
+                casvi.CanRotate = true;
+                casvi.CanScale = true;
+                casvi.Name = "DraggedClipArt" + curIndex;
+                casvi.Center = e.Cursor.GetPosition(scatterView);
+                casvi.Orientation = e.Cursor.GetOrientation(ClipArtList); // need to find which window dragged this object
+
+
+                // Add to global tracker of clipart spawned, then add to screen
+                curClipArt.Add(casvi);
+                scatterView.Items.Add(casvi);
+
+                //sender.SetIsItemDataEnabled(e.Cursor.Data, true);
+
             }
             // CJT this is needed to re-enable the item in the library!!
-            BackgroundList.SetIsItemDataEnabled(e.Cursor.Data, true);
+            //BackgroundList.SetIsItemDataEnabled(e.Cursor.Data, true);
+            ClipArtList.SetIsItemDataEnabled(e.Cursor.Data, true); // need to do for all current clipart bars that are open OR look for which one dragged it here
         }
 
         /// <summary>
@@ -199,7 +296,7 @@ namespace MyScene
             ClipArtListView.SetRelativeZIndex(RelativeScatterViewZIndex.Topmost);
         }
 
-        
+
         void BottomButton1Clicked(object sender, RoutedEventArgs e)
         {
             ClipArtListView.Visibility = System.Windows.Visibility.Visible;
@@ -214,6 +311,30 @@ namespace MyScene
             ClipArtListView.SetRelativeZIndex(RelativeScatterViewZIndex.Topmost);
         }
         #endregion Sidebar Code
+
+        // CJT created for removing the item when dropped on the clipart window -- needs some more work
+        private void ClipArtListView_Drop(object sender, SurfaceDragDropEventArgs e)
+        {
+            // find the path information for the dragged object
+            FrameworkElement findSource = (e.Cursor.Visual) as FrameworkElement;
+            ScatterViewItem draggedElement = null;
+            while (draggedElement == null && findSource != null)
+            {
+                if ((draggedElement = findSource as ScatterViewItem) == null)
+                {
+                    findSource = VisualTreeHelper.GetChild(findSource, 0) as FrameworkElement;
+                }
+            }
+
+            if (draggedElement == null)
+            {
+                return;
+            }
+
+            //string ImagePath = draggedElement.Source.ToString();
+            scatterView.Items.Remove(draggedElement); // remove from screen
+            curClipArt.Remove(draggedElement); // remove from list
+        }
 
     }
 }
